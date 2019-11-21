22640be76be36cbb496fa4667d9e8bc69b71267d
faheem sajjad <faheem.sajjad@zameen.com>
2019-11-21 16:55:51 +0500

    testing eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
+    env: {
+        browser: true,
+        es6: true
     },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
+    extends: ['loopback','prettier'],
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly"
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module"
+    },
+    rules: {}
 };
--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 //Load express module with `require` directive
-var express = require('express')
-var app = express()
+// eslint-disable-next-line no-undef
+var express = require("express");
+var app = express();
 
 //Define request response in root URL (/)
-app.get('/', function (req, res) {
-	res.send('Hello World')
-})
+app.get("/", function(req, res) {
+res.send("Hello World");
+});
 
 //Launch listening server on port 3000
-app.listen(3000, function () {
-	console.log('App listening on port 3000!')
-})
+app.listen(3000, function() {
+console.log("App listening on port 3000!");
+});
